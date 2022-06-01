39c05c806c07a702800c39c858e08a1223ad67e8
alexanderelenin <elenin.workspace@gmail.com>
2022-06-01 14:14:35 +0400

    first commit

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,18 @@
+import classes from './MealsSummary.module.css';
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Your Meal = Our deal</h2>
+      <p>
+        Choose your favorite meal, fall back and enjoy. All of that remotely
+      </p>
+      <p>
+        This experience will leave you no chance to alter your choices while
+        ordering food. Enjoy!
+      </p>
+    </section>
+  );
+};
+
+export default MealsSummary;
